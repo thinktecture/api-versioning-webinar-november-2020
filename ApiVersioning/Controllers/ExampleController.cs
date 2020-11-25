0924f9b45397b819b2d04aef3b30c62e1101d357
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-11-25 08:12:33 +0100

    add new controller with endpoints for multiple versions

---
+++ ApiVersioning/Controllers/ExampleController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioning
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiVersion("1.0"), ApiVersion("2.0")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet("Version")]
+        [MapToApiVersion("1.0")]
+        public ActionResult<string> Version()
+        {
+            return Ok("1");
+        }
+
+        [HttpGet("Version")]
+        [MapToApiVersion("2.0")]
+        public ActionResult<string> Version2()
+        {
+            return Ok("2");
+        }
+    }
+}
